ac51f463ba3291370766a2ba6472b5f1a87cbccd
Neyen Frandino <neyenfrandino1@gmail.com>
2024-11-26 09:51:32 -0300

    Configuramos las nuevas credenciales db

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -49,5 +49,5 @@ app.include_router(cost_analytics.router)
 
 
 
-# if __name__ == "__main__":
-#    uvicorn.run('main:app', port=8000, reload=True)
+if __name__ == "__main__":
+   uvicorn.run('main:app', port=8000, reload=True)
