d2271dfb545f5c01a87dcd63cad392285cb82814
Neyen Frandino <neyenfrandino1@gmail.com>
2024-12-27 10:39:02 -0300

    conectando con el backend y la base de datos

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -16,36 +16,36 @@ from app.routers.analytics.sell_analysis import sell_total_analytics
 from app.routers.analytics.cost_analysis import cost_analytics
 
 
-# from app.db.database import Base, engine 
+from app.db.database import Base, engine 
 
 app = FastAPI()
 
 
 # Configurar CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],  # Agrega PATCH a la lista de métodos permitidos,
-    allow_headers=["*"],
-)
-
-# Permitir CORS para el frontend en Render
 # app.add_middleware(
 #     CORSMiddleware,
-#     allow_origins=[
-#         "https://creaciones-vuela.netlify.app",  # Tu frontend en Netlify
-#     ],
+#     allow_origins=["http://localhost:3000"],
 #     allow_credentials=True,
-#     allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
+#     allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],  # Agrega PATCH a la lista de métodos permitidos,
 #     allow_headers=["*"],
 # )
 
+# Permitir CORS para el frontend en Render
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://creaciones-vuela.netlify.app",  # Tu frontend en Netlify
+    ],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allow_headers=["*"],
+)
 
-# def create_tables():
-#     Base.metadata.create_all(bind=engine)
 
-# create_tables()
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+create_tables()
 
 app.include_router(auth.router)
 app.include_router(user.router)
@@ -62,9 +62,9 @@ app.include_router(cost_analytics.router)
 
 
 
-if __name__ == "__main__":
-   uvicorn.run('main:app', port=8000, reload=True)
-
 # if __name__ == "__main__":
-#     port = int(os.getenv('POSTGRES_PORT'))  # Obtener el puerto desde la variable de entorno, con 8000 como valor por defecto
-#     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+#    uvicorn.run('main:app', port=8000, reload=True)
+
+if __name__ == "__main__":
+    port = int(os.getenv('POSTGRES_PORT'))  # Obtener el puerto desde la variable de entorno, con 8000 como valor por defecto
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
